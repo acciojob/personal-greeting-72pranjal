75d57f47b3c57cd35b7771965e26074b998cc700
Pranjal gupta <pranjalgupta.gh@gmail.com>
2025-02-17 12:53:36 +0530

    done

---
+++ src/components/Greeting.jsx
@@ -0,0 +1,18 @@
+import React, {useState} from 'react'
+
+const Greeting = () => {
+  let [name, setName] = useState('')
+  const handleChange = (e) => {
+    setName(e.target.value);
+  }
+  return (
+    <div>
+        <label htmlFor="name">Enter your name:</label>
+        <br /> <br />
+        <input type="text" onChange={handleChange} />
+        <p>{name.trim() !== '' ? `Hello, ${name}!`: ''}</p>
+    </div>
+  )
+}
+
+export default Greeting
